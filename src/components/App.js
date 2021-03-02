b9f3b11e81818486102ef8a4386c20701125ec03
Shikha14DoD <34702351+Shikha14DoD@users.noreply.github.com>
2021-03-02 10:50:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,36 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+    constructor()
+  {
+    super()
+    this.state={
+      time:(new Date().toLocaleTimeString()) 
+    }
+  }
+    
+    componentDidMount(){
+    
+    this.interval=setInterval(()=>{
+      this.setState({time:(new Date().toLocaleTimeString())})
+    },1000)
+  }
+
+
+  componentWillUnmount()
+  {
+    clearInterval(this.interval);
+  }
+
     render() {
 
         return(
             <>
-               
+               <div className="Clock">
+        <h3 id="time">
+        {this.state.time}
+        </h3>
+        </div>
             </>
         )
     }
